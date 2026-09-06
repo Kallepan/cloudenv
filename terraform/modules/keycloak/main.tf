62df3022e6eaed5d5e8d81e42face5fbf55e05ac
Kajeepan Kandeepan <kajeepan.kandeepan@gmail.com>
2026-09-06 09:09:41 +0200

    feat: add local provider configuration for Keycloak module

--- terraform/modules/keycloak/main.tf
+++ terraform/modules/keycloak/main.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
     docker = {
       source  = "kreuzwerker/docker"
       version = "~> 3.0"
